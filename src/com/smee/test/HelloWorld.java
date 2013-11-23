c5ee1bf79649bdd57775575ad7576d9e25669e40
崔端午 <duanmutengwan@gmail.com>
2013-11-23 08:27:45 -0800

    初步Git测试

---
+++ src/com/smee/test/HelloWorld.java
@@ -0,0 +1,35 @@
+/*
+* Copyright (c) 2012-2020 Founder duanmutengwan. All Rights Reserved.
+*
+* This software is the confidential and proprietary information of
+* Founder. You shall not disclose such Confidential Information
+* and shall use it only in accordance with the terms of the agreements
+* you entered into with Founder.
+*
+*/
+package com.smee.test;
+
+/**
+ * This class is used for ...
+ *
+ * @version
+ *       1.0, 2013-11-23 下午11:17:33
+ * @author  崔端午
+ */
+public class HelloWorld {
+
+	/**
+	 * @Title: main
+	 * @Description: TODO(这里用一句话描述这个方法的作用)
+	 * @param @param args    设定文件
+	 * @return void    返回类型
+	 * @throws 
+	 * @author 崔端午
+	 */
+	public static void main(String[] args) {
+		System.out.println("第一个Git项目测试发布");
+
+	}
+
+}
+ 
